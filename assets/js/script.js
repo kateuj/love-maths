e00cd38fc19014e7c9ad1ef7062a8c8cedaa2a8a
kateuj <kate.ulloajames@gmail.com>
2023-11-10 09:01:41 +0000

    question function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1'). textContent = operand1;
+    document.getElementById('operand2'). textContent = operand2;
+    document.getElementById('operator'). textContent = operator;
 
 }
 
